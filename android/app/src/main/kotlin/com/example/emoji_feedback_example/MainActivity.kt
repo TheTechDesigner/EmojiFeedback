6daedead52204a8454923a38c90f3c8c345ab9ee
MohammedAliKadiwal <ali@silverwebbuzz.com>
2020-09-27 17:22:34 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/emoji_feedback_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.emoji_feedback_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:emoji_feedback/emoji_feedback.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFFC41A3B),
+        primaryColorLight: Color(0xFFFBE0E6),
+        accentColor: Color(0xFF1B1F32),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String title = 'Emoji Feedback';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Text(
+                'How did you like this video ?',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFC41A3B),
+                ),
+              ),
+            ),
+            EmojiFeedback(
+              onChange: (index) {
+                print(index);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
